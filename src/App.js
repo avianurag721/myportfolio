c0704d17f2d4c61201e6561b1b2e9559224048fe
avianurag721 <anurag.721721@gmail.com>
2024-01-01 14:54:25 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <div className="w-[100%] mt-4 bg-slate-700 rounded-t-md font-serif border-b-2 text-black lg:w-[80%]">
           <Navbar />
         </div>
-        <div className="w-[100%] bg-slate-700 text-black lg:w-[80%]">
+        <div className="w-[90%] bg-slate-700 text-black lg:w-[80%]">
 
           <Home />
           <About />
